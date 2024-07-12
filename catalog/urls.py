4bbb32fd3071f40c94a2ae90a1e5b07c225d9143
konys <Konyshev_PV@bk.ru>
2024-07-12 18:51:32 +0300

    dz23.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,19 +1,22 @@
 from django.conf import settings
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from .apps import CatalogConfig
 from .views import ProductListView, ProductDetailView, ContactPageView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView, toggle_in_stock
+    ProductDeleteView, toggle_in_stock, CategoryListView
 from django.conf.urls.static import static
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="product_list"),
     path("contacts/", ContactPageView.as_view(), name="contacts"),
-    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("products/<int:pk>/", cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
-    path('activity/<int:pk>/delete/', toggle_in_stock, name='toggle_in_stock')
+    path('activity/<int:pk>/delete/', toggle_in_stock, name='toggle_in_stock'),
+    path('categories/', CategoryListView.as_view(), name='categories_list')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
